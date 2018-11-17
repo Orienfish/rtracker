b6bacad18fd11dd7c161d1cf477333de9fe7527b
OG <oliver.ganguli@gmail.com>
2018-11-17 13:37:01 -0800

    initial attempt at extension websocket.

--- Browser/socket.js
+++ Browser/socket.js
@@ -1,15 +1,41 @@
+
+
 // Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8080');
+// console.log('is this working?');
+// const socket = new WebSocket('ws://localhost:8080');
 
-console.log('is this working?')
+// // Connection opened
+// socket.addEventListener('open', function (event) {
+//     socket.send('Hello Server!');
+//     console.log('connection opened');
+// });
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-    console.log('is this working?')
+// // Listen for messages
+// socket.addEventListener('message', function (event) {
+//     console.log('Message from server ', event.data);
+// });
+
+// Create SocketIO instance, connect
+var socket = new io.Socket('localhost',{
+	port: 8080
+});
+socket.connect(); 
+
+// Add a connect listener
+socket.on('connect',function() {
+	console.log('Client has connected to the server!');
+});
+// Add a connect listener
+socket.on('message',function(data) {
+	console.log('Received a message from the server!',data);
+});
+// Add a disconnect listener
+socket.on('disconnect',function() {
+	console.log('The client has disconnected!');
 });
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-});
+// Sends a message to the server via sockets
+function sendMessageToServer(message) {
+	socket.send(message);
+}
+
