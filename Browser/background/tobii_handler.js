bce612128d1537557055720a842a941c87d88af0
OG <oliver.ganguli@gmail.com>
2018-11-28 11:23:08 -0800

    starting to work on the background script

---
+++ Browser/background/tobii_handler.js
@@ -0,0 +1,7 @@
+//TODO: implement the background communication with content scripts
+
+// create the websocket content
+
+// figure out how to sent that content to the events page 
+
+// create the necessary permissions
