8dd16fcca0ee8050f3b42e2dfd1cfa9b6dd73004
m2olsen <m2olsen@ucsd.edu>
2018-12-05 22:09:41 -0800

    Properly using asynchronous calls.
    Got the asynchronous call functionality working on the browser and removed hard-coded syllableDict. All I have left to do is to potentially have it retrieve definitions and display those.

--- Browser/Syllables.js
+++ Browser/Syllables.js
@@ -1,6 +1,3 @@
-//const { require } = Cu.import("resource://gre/modules/commonjs/toolkit/require.js", {})
-//const https = require('https');
-//const fs = require('fs');
 const key = "f5b9a3a3-0560-4ea5-8a8d-de8e489336c5";
 const conversionDict = {
     "AA" : "ɑ",
@@ -55,108 +52,65 @@ const conversionDict = {
     "Z"  : "z",
     "ZH" : "ʒ"
 };
-let syllableDict = {'the': { 'pronunciation': [ [ 'ð', 'ʌ' ] ], 'syllables': [ 'the' ] },
-                    'culmination': { 'pronunciation': [ [ 'k', 'ʌ', 'l' ], [ 'm', 'ʌ' ], [ 'n', 'eɪ' ], [ 'ʃ', 'ʌ', 'n' ] ], 'syllables': [ 'cul', 'mi', 'na', 'tion' ] },
-                    'of': { 'pronunciation': [ [ 'ʌ', 'v' ] ], 'syllables': [ 'of' ] },
-                    'speech': { 'pronunciation': [ [ 's', 'p', 'i', 'tʃ' ] ], 'syllables': [ 'speech' ] },
-                    'for': { 'pronunciation': [ [ 'f', 'ɔ', 'ɹ' ] ], 'syllables': [ 'for' ] },
-                    'which': { 'pronunciation': [ [ 'w', 'ɪ', 'tʃ' ] ], 'syllables': [ 'which' ] },
-                    'i': { 'pronunciation': [ [ 'aɪ' ] ], 'syllables': [ 'i' ] },
-                    'labored': { 'pronunciation': [ [ 'l', 'eɪ' ], [ 'b', 'ɝ', 'd' ] ], 'syllables': [ 'la', 'bored' ] },
-                    'was': { 'pronunciation': [ [ 'w', 'ɑ', 'z' ] ], 'syllables': [ 'was' ] },
-                    'often': { 'pronunciation': [ [ 'ɔ' ], [ 'f', 'ʌ', 'n' ] ], 'syllables': [ 'of', 'ten' ] },
-                    'criticism': { 'pronunciation': [ [ 'k', 'ɹ', 'ɪ' ], [ 't', 'ɪ' ], [ 's', 'ɪ' ], [ 'z', 'ʌ', 'm' ] ], 'syllables': [ 'crit', 'i', 'cism' ] },
-                    'bored': { 'pronunciation': [ [ 'b', 'ɔ', 'ɹ', 'd' ] ], 'syllables': [ 'bored' ] },
-                    'expressions': { 'pronunciation': [ [ 'ɪ', 'k', 's', 'p' ], [ 'ɹ', 'ɛ' ], [ 'ʃ', 'ʌ', 'n', 'z' ] ], 'syllables': [ 'ex', 'pres', 'sion' ] },
-                    'and': { 'pronunciation': [ [ 'ʌ', 'n', 'd' ] ], 'syllables': [ 'and' ] },
-                    'sometimes': { 'pronunciation': [ [ 's', 'ʌ', 'm' ], [ 't', 'aɪ', 'm', 'z' ] ], 'syllables': [ 'some', 'times' ] },
-                    'outright': { 'pronunciation': [ [ 'aʊ', 't' ], [ 'ɹ', 'aɪ', 't' ] ], 'syllables': [ 'out', 'right' ] },
-                    'rejection': { 'pronunciation': [ [ 'ɹ', 'ɪ' ], [ 'dʒ', 'ɛ', 'k' ], [ 'ʃ', 'ʌ', 'n' ] ], 'syllables': [ 're', 'jec', 'tion' ] },
-                    'thus': { 'pronunciation': [ [ 'ð', 'ʌ', 's' ] ], 'syllables': [ 'thus' ] },
-                    'after': { 'pronunciation': [ [ 'æ', 'f' ], [ 't', 'ɝ' ] ], 'syllables': [ 'af', 'ter' ] },
-                    'unsuccessful': { 'pronunciation': [ [ 'ʌ', 'n' ], [ 's', 'ʌ', 'k' ], [ 's', 'ɛ', 's' ], [ 'f', 'ʌ', 'l' ] ], 'syllables': [ 'un', 'suc', 'cess', 'ful' ] },
-                    'revisions': { 'pronunciation': [ [ 'ɹ', 'i' ], [ 'v', 'ɪ' ], [ 'ʒ', 'ʌ', 'n', 'z' ] ], 'syllables': [ 're', 'vi', 'sion' ] },
-                    'heartfelt': { 'pronunciation': [ [ 'h', 'ɑ', 'ɹ', 't' ], [ 'f', 'ɛ', 'l', 't' ] ], 'syllables': [ 'heart', 'felt' ] },
-                    'considerations': { 'pronunciation': [  [ 'k', 'ʌ', 'n' ], [ 's', 'ɪ' ], [ 'd', 'ɝ' ], [ 'eɪ' ], [ 'ʃ', 'ʌ', 'n', 'z' ] ], 'syllables': [ 'con', 'sid', 'er', 'a', 'tion' ] },
-                    'came': { 'pronunciation': [ [ 'k', 'eɪ', 'm' ] ], 'syllables': [ 'came' ] },
-                    'to': { 'pronunciation': [ [ 't', 'u' ] ], 'syllables': [ 'to' ] },
-                    'a': { 'pronunciation': [ [ 'ʌ' ] ], 'syllables': [ 'a' ] },
-                    'conclusion': { 'pronunciation': [ [ 'k', 'ʌ', 'n', 'k' ], [ 'l', 'u' ], [ 'ʒ', 'ʌ', 'n' ] ], 'syllables': [ 'con', 'clu', 'sion' ] },
-                    'no': { 'pronunciation': [ [ 'n', 'oʊ' ] ], 'syllables': [ 'no' ] },
-                    'radical': { 'pronunciation': [ [ 'ɹ', 'æ' ], [ 'd', 'ʌ' ], [ 'k', 'ʌ', 'l' ] ], 'syllables': [ 'rad', 'i', 'cal' ] },
-                    'idea': { 'pronunciation': [ [ 'aɪ' ], [ 'd', 'i' ], [ 'ʌ' ] ], 'syllables': [ 'idea' ] },
-                    'however': { 'pronunciation':[ [ 'h', 'aʊ' ], [ 'ɛ' ], [ 'v', 'ɝ' ] ], 'syllables': [ 'how', 'ev', 'er' ] },
-                    'expertly': { 'pronunciation':[ [ 'ɛ', 'k', 's' ], [ 'p', 'ɝ', 't' ], [ 'l', 'i' ] ], 'syllables': [ 'ex', 'pert', 'ly' ] },
-                    'or': { 'pronunciation':[ [ 'ɔ', 'ɹ' ] ], 'syllables': [ 'or' ] },
-                    'clumsily': { 'pronunciation':[ [ 'k', 'l', 'ʌ', 'm' ], [ 's', 'ʌ' ], [ 'l', 'i' ] ], 'syllables': [ 'clum', 'sy' ] },
-                    'delivered': { 'pronunciation':[ [ 'd', 'ɪ' ], [ 'l', 'ɪ' ], [ 'v', 'ɝ', 'd' ] ], 'syllables': [ 'de', 'liv', 'er' ] },
-                    'written': { 'pronunciation':[ [ 'ɹ', 'ɪ' ], [ 't', 'ʌ', 'n' ] ], 'syllables': [ 'write' ] },
-                    'will': { 'pronunciation':[ [ 'w', 'ɪ', 'l' ] ], 'syllables': [ 'will' ] },
-                    'be': { 'pronunciation':[ [ 'b', 'i' ] ], 'syllables': [ 'be' ] },
-                    'unanimously': { 'pronunciation':[ [ 'j', 'u' ], [ 'n', 'æ' ], [ 'n', 'ʌ' ], [ 'm', 'ʌ', 's' ], [ 'l', 'i' ] ], 'syllables': [ 'unan', 'i', 'mous' ] },
-                    'accepted': { 'pronunciation': [ [ 'æ', 'k' ], [ 's', 'ɛ', 'p' ], [ 't', 'ɪ', 'd' ] ], 'syllables': [ 'ac', 'cept', 'ed' ] },
-                    'instead': { 'pronunciation':[ [ 'ɪ', 'n', 's' ], [ 't', 'ɛ', 'd' ] ], 'syllables': [ 'in', 'stead' ] },
-                    'ideas': { 'pronunciation':[ [ 'aɪ' ], [ 'd', 'i' ], [ 'ʌ', 'z' ] ], 'syllables': [ 'idea' ] },
-                    'encounter': { 'pronunciation':[ [ 'ɪ', 'n' ], [ 'k', 'aʊ', 'n' ], [ 't', 'ɝ' ] ], 'syllables': [ 'en', 'coun', 'ter' ] },
-                    'without': { 'pronunciation':[ [ 'w', 'ɪ' ], [ 'θ', 'aʊ', 't' ] ], 'syllables': [ 'with', 'out' ] },
-                    'constructive': { 'pronunciation':[ [ 'k', 'ʌ', 'n', 's', 't' ], [ 'ɹ', 'ʌ', 'k' ], [ 't', 'ɪ', 'v' ] ], 'syllables': [ 'con', 'struc', 'tive' ] },
-                    'comment': { 'pronunciation':[ [ 'k', 'ɑ' ], [ 'm', 'ɛ', 'n', 't' ] ], 'syllables': [ 'com', 'ment' ] },
-                    'but': { 'pronunciation':[ [ 'b', 'ʌ', 't' ] ], 'syllables': [ 'but' ] },
-                    'this': { 'pronunciation':[ [ 'ð', 'ɪ', 's' ] ], 'syllables': [ 'this' ] },
-                    'fact': { 'pronunciation':[ [ 'f', 'æ', 'k', 't' ] ], 'syllables': [ 'fact' ] },
-                    'does': { 'pronunciation':[ [ 'd', 'ʌ', 'z' ] ], 'syllables': [ 'does' ] },
-                    'not': { 'pronunciation':[ [ 'n', 'ɑ', 't' ] ], 'syllables': [ 'not' ] },
-                    'negate': { 'pronunciation':[ [ 'n', 'ɪ' ], [ 'ɡ', 'eɪ', 't' ] ], 'syllables': [ 'ne', 'gate' ] },
-                    'our': { 'pronunciation':[ [ 'aʊ' ], [ 'ɝ' ] ], 'syllables': [ 'our' ] },
-                    'responsibility': { 'pronunciation':[ [ 'ɹ', 'i', 's' ], [ 'p', 'ɑ', 'n' ], [ 's', 'ʌ' ], [ 'b', 'ɪ' ], [ 'l', 'ʌ' ], [ 't', 'i' ] ], 'syllables': [ 're', 'spon', 'si', 'bil', 'i', 'ty' ] },
-                    'write': { 'pronunciation':[ [ 'ɹ', 'aɪ', 't' ] ], 'syllables': [ 'write' ] },
-                    'them': { 'pronunciation':[ [ 'ð', 'ɛ', 'm' ] ], 'syllables': [ 'them' ] },
-                    'take': { 'pronunciation':[ [ 't', 'eɪ', 'k' ] ], 'syllables': [ 'take' ] },
-                    'stand': { 'pronunciation':[ [ 's', 't', 'æ', 'n', 'd' ] ], 'syllables': [ 'stand' ] } };
-/*let arpabetDict = {};
-
-fs.readFile('data.json', (err, data) => {
-    if (err) throw err;
-    arpabetDict = JSON.parse(data);
-});*/
-
+let syllableDict = {};
 
 function ArpabetToIpa(phonetic) {
     return conversionDict[phonetic];
 }
 
-async function RequestWordData(word) {
+// checking so see if I can have this outside the function
+// this would prevent new objects being initialized every call
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback){
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function(){
+            if(anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send(null);
+    }
+}
+var client = new HttpClient();
+
+// http request made possible due to:
+// https://www.youtube.com/watch?v=lD1iRp4ewZY&ab_channel=jinujawadm
+async function RequestWordData(word, callback) {
     const url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=" + key;
+    
+    syllableDict[word] = {};
+    GetPronunciation(word);
 
-    // request for data from Merriam-Webster API
-    // request code used from: https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
-    https.get(url, (resp) => {
-        let data = '';
+    client.get(url, function(response) {
+        var response1 = JSON.parse(response);
+        let syllables = response1[0]['hwi']['hw'].split('*');
         
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
+        //TODO: more acurately parse the response from Merriam-Webster
+        //          make sure that you're using an index that has the right tense of the word
+        syllableDict[word]['syllables'] = syllables;
 
-        resp.on('end', () => {
-            let response = JSON.parse(data);
-            let syllables = response[0]['hwi']['hw'].split('*');
-            console.log(syllables);
-            syllableDict[word]['syllables'] = syllables;
-            //console.log('Syllables set for: ' + word);
-            console.log(syllableDict);
-            
-            //potentially add return value
-            /*let value = {};
-            value['word'] = word;
-            value['syllables'] = syllables;
-            value['pronunciation'] = "";*/
-            
-            //return value;
-            //TODO: add event representing it is finished?
-        });
-    }).on('error', (err) => {
-        console.log('Error: ' + err.message);
-    });
+        let definitions = [];
+        //TODO: retrieve all the definitions of the word in the correct tense
+        syllableDict[word]['definitions'] = definitions;
+
+        let value = wordToJSON(word);
+        
+        console.log(value);
+        console.log(response1);
+
+        console.log('calling callback in RequestWordData');
+        callback(value);
+    })
+}
+
+function wordToJSON(word){
+    let value = {};
+    value['syllablePopup'] = {};
+    value['syllablePopup']['word'] = word;
+    value['syllablePopup']['syllables'] = syllableDict[word]['syllables'];
+    value['syllablePopup']['pronunciation'] = syllableDict[word]['pronunciation'];
+    //TODO: value['syllablePopup']['definitions'] = definitions;
+    return value;
 }
 
 function GetPronunciation(word){
@@ -181,49 +135,17 @@ function GetPronunciation(word){
     console.log(pronun);
     syllableDict[word]['pronunciation'] = pronun;
     return pronun;
-    //console.log('Pronunciation set for: ' + word);
-    //console.log('ipa = ' + ipa);
 }
 
-function AddWordToDict(word){
+function GetWordFromDict(word, callback){
     // check to make sure the selected word isn't already stored
     if(word in syllableDict){
         console.log('Data already retreived for: ' + word);
+        let value = wordToJSON(word);
+        callback(value);
         return;
     }
     
-    syllableDict[word] = {};
-    
-    RequestWordData(word);
-    GetPronunciation(word);
+    console.log('Calling RequestWordData');
+    RequestWordData(word, callback);
 }
-
-// Test to make sure that the function is working
-function test(word){
-    console.log(word+':');
-    AddWordToDict(word);
-}
-
-//for prototyping to mitigate importing of outside libraries and local files
-function workAround(){
-    let list = ['the', 'culmination', 'of', 'the', 'progressionist', 'speech', 'for', 'which', 'I', 'labored', 'was', 'often', 'criticism', 'bored', 'expressions', 'and', 'sometimes', 'outright', 'rejection', 'thus', 'after', 'unsuccessful', 'revisions', 'and', 'heartfelt', 'considerations', 'came', 'to', 'a', 'conclusion', 'no', 'radical', 'idea', 'however', 'expertly', 'or', 'clumsily', 'delivered', 'written', 'will', 'be', 'unanimously', 'accepted', 'instead', 'radical', 'ideas', 'will', 'often', 'encounter', 'criticism', 'without', 'constructive', 'comment', 'but', 'this', 'fact', 'does', 'not', 'negate', 'our', 'responsibility', 'to', 'write', 'them', 'take', 'a', 'stand'];
-    list.forEach(function(word) {
-        test(word);
-    });
-}
-
-function getSyllables(word){
-    console.log('i get here?');
-    if(word in syllableDict){
-        return syllableDict[word];
-    }
-
-    console.log("Could not retrieve syllables for: " + word);
-    return undefined;
-}
-
-//setTimeout(workAround, 1000);
-//setTimeout(test, 1000, 'stand');
-//setTimeout(test, 1000, 'literally');
-//console.log(arpabetDict['really']);
-//console.log(syllableDict['responsibility']);
--- Browser/background.js
+++ Browser/background.js
@@ -11,36 +11,18 @@ function connectPopup(p) {
     portFromCS = p;
     portFromCS.postMessage({ greeting: "hi there content script!" });
     console.log('background port opened');
-    /*portFromCS.onMessage.addListener(function (m) {
-        console.log(m.greeting);
-    });*/
 }
 
 
 function contextMenuAction(info, tab){
     if (info.menuItemId == "syllables") {
       const word = info.selectionText.replace(/\s+/, "");
-      AddWordToDict(word);
-      let syllableInfo = getSyllables(word);
-      if(syllableInfo === undefined){
-          console.log('Failed to retrieve syllable content for: ' + word);
-      } else{
-        console.log('ARPABET test:');
-        console.log(arpabetDict[word]);
-        portFromCS.postMessage({
-            'syllablePopup': {
-                'word': word,
-                'syllables': syllableInfo['syllables'],
-                'pronunciation': syllableInfo['pronunciation']
-            }
-        });
-        /*console.log('info from background.js');
-        console.log(word);
-        console.log(syllableInfo['syllables']);
-        console.log(syllableInfo['pronunciation']);*/
-      }
       
-    } else {
-        console.log('whoops');
+      // return stored info or look it up
+      GetWordFromDict(word, syllableCallback);
     }
+}
+
+function syllableCallback(value){
+    portFromCS.postMessage(value);
 }
